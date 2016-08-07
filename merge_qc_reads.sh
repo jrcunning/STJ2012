d45054d747db7820d9bf36a61d3b2b00f404aa32
jrcunning <ross.cunning@gmail.com>
2016-08-07 10:10:11 -1000

    trim primer sequences

--- merge_qc_reads.sh
+++ merge_qc_reads.sh
@@ -20,3 +20,19 @@ add_qiime_labels.py -m data/mapping_file.txt -i data/merge/ -c InputFileName -o 
 # Chimera checking
 identify_chimeric_seqs.py -i data/fasta/combined_seqs.fna --suppress_usearch61_ref -m usearch61 -o data/fasta/usearch61_chimeras
 filter_fasta.py -f data/fasta/combined_seqs.fna -o data/fasta/combined_seqs_chimera_filtered.fasta -s data/fasta/usearch61_chimeras/chimeras.txt -n
+
+# Trim primer sequences using cutadapt
+# Trim forward primers (allow error rate of 15% (3 indels/mismatches))
+cutadapt -g GTGAATTGCAGAACTCCGTG -e 0.15 data/fasta/combined_seqs_chimera_filtered.fasta -o data/fasta/trimF.fasta
+# Trim forward primers again (may be multiple internal primer sequences), but do not discard sequences that do not contain primer
+cutadapt -g GTGAATTGCAGAACTCCGTG -e 0.15 data/fasta/trimF.fasta -o data/fasta/trimF2.fasta
+# Trim reverse primers using cutadapt
+#   Do not remove sequences that do not have reverse primer sequence
+cutadapt -a AAGCATATAAGTAAGCGGAGG -e 0.15 data/fasta/trimF2.fasta -o data/fasta/trimF2_trimR.fasta
+# Trim reverse primers again for any remaining internally
+cutadapt -a AAGCATATAAGTAAGCGGAGG -e 0.15 data/fasta/trimF2_trimR.fasta -o data/fasta/combined_seqs_trimmed.fasta
+
+# Remove intermediate files
+rm data/fasta/trimF.fasta
+rm data/fasta/trimF2.fasta
+rm data/fasta/trimF2_trimR.fasta
