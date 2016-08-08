e0a9607cd4670e3d946a0138b45bf68f41f30c3d
Ross Cunning <ross.cunning@gmail.com>
2016-08-08 08:26:50 -1000

    run 97% global clustering

--- bioinformatics_run.sh
+++ bioinformatics_run.sh
@@ -5,14 +5,32 @@
 # Merge, QC, and trim Illumina reads
 ./merge_qc_reads.sh
 
+
+
+# -------------------------------
 # Cluster 97% OTUs within samples
 ./otus_97.sh
 
-# Assign taxonomy and generate OTU table for 97% OTUs
-./tax_results.sh data/otus_97/all_rep_set_rep_set.fasta 97_otus
+# Assign taxonomy and generate OTU table for 97% within-sample OTUs
+./tax_results.sh data/otus_97/97_otus_rep_set.fasta 97_otus
+# -------------------------------
 
+
+
+# -------------------------------
+# Cluster 97% OTUs within samples
+./otus_97_bysample.sh
+
+# Assign taxonomy and generate OTU table for 97% within-sample OTUs
+./tax_results.sh data/otus_97_bysample/all_rep_set_rep_set.fasta 97_otus
+# -------------------------------
+
+
+
+# -------------------------------
 # Cluster 100% OTUs
 ./otus_100.sh
 
 # Assign taxonomy and generate OTU table for 97% OTUs
-./tax_results.sh data/otus_100/rep_set.fasta 100_otus
+./tax_results.sh data/otus_100/100_otus_rep_set.fasta 100_otus
+# -------------------------------
