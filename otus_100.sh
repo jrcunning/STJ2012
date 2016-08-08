17b8e431aea70ba2ac94a08edcba671f78e82c23
Ross Cunning <ross.cunning@gmail.com>
2016-08-08 08:27:31 -1000

    remove old code

--- otus_100.sh
+++ otus_100.sh
@@ -9,24 +9,4 @@ awk '$3 ~ /./ {print}' data/otus_100/combined_seqs_chimera_filtered_otus.txt > d
 # Get rep set of 100% OTUs
 pick_rep_set.py -i data/otus_100/nosingles_otus.txt \
 -f data/fasta/combined_seqs_chimera_filtered.fasta \
--o data/otus_100/rep_set.fasta
-
-# Assign taxonomy
-#assign_taxonomy.py -i data/otus_100/rep_set.fasta -m blast \
-#-r data/ITS2db.fasta \
-#-t data/id_to_taxonomy.txt \
-#-o data/otus_100/blast_taxonomy
-
-## Make list of "no blast hits"
-#awk '/No blast hit/' data/otus_100/blast_taxonomy/rep_set_tax_assignments.txt > data/otus_100/blast_taxonomy/no_blast_hits.txt
-
-## Make OTU table excluding no blast hits
-#make_otu_table.py -i data/otus_100/nosingles_otus.txt \
-#-t data/otus_100/blast_taxonomy/rep_set_tax_assignments.txt \
-#-e data/otus_100/blast_taxonomy/no_blast_hits.txt \
-#-o data/otus_100/100_otu_table.biom
-#rm data/100_otu_table.tsv  # Delete old OTU .tsv if present
-#biom convert -i data/otus_100/100_otu_table.biom -o data/100_otu_table.tsv --to-tsv
-
-## Add clade to tax table and copy to data directory
-#cut -d$'\t' -f4- data/otus_100/blast_taxonomy/rep_set_tax_assignments.txt | cut -c-1 | paste - data/otus_100/blast_taxonomy/rep_set_tax_assignments.txt > data/100_tax_assignments.txt
+-o data/otus_100/100_otus_rep_set.fasta
