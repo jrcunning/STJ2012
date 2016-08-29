234fd7abf3bdcf2164c9ec9daea24d222f4100d4
Ross Cunning <ross.cunning@gmail.com>
2016-08-29 07:19:58 -0700

    reformat nw output

--- R/run_nw.R
+++ R/run_nw.R
@@ -1,9 +1,8 @@
-# get command line arguments
+# Get command line arguments
 args = commandArgs(trailingOnly=TRUE)
-
-# test if there is at least one argument: if not, return an error
+# If two arguments not provided, return an error
 if (length(args) < 2) {
-  stop("must specify query sequences and reference sequences", call.=FALSE)
+  stop("must specify query sequences (.fasta) and reference sequences (.fasta)", call.=FALSE)
 }
 
 # Import query sequences
@@ -17,7 +16,13 @@ ref <- readDNAStringSet(args[2])
 # import substitution matrix
 EDNAFULL <- as.matrix(read.table("data/EDNAFULL", skip=8, header=T, row.names=1))
 
-# Build function to find which reference sequences produces best alignment score
+# Build function to find which reference sequences produces best alignment score using
+# Needleman-Wunsch algorithm with default EMBOSS parameters for DNA sequences.
+# -type="global-local" forces whole reference (pattern) sequence to be used, but subject/query may be 
+# a concescutive subsequence. This is because queries may include slightly longer region of gene 
+# than references because references were trimmed by o-smart-trim. This means that query sequence 
+# can have more bases than the reference at beginning or end of the sequence (=end gaps in 
+# alignment) without penalties.
 get.best <- function(x) {
   scores <- pairwiseAlignment(subject=x, pattern=ref, type="global-local", scoreOnly=TRUE,
                            substitutionMatrix=EDNAFULL,
@@ -37,26 +42,23 @@ best <- parSapply(cl, otus, FUN=get.best)
 stopCluster(cl)  # Stop cluster
 
 # Collect results in a data frame
-results <- data.frame(otu=names(otus), hits=best, stringsAsFactors = FALSE)
-# If multiple hit, select first one
-results$hit <- unlist(lapply(strsplit(results$hits, split=";"), "[", 1))
-#results$hit <- gsub(";;", ";", paste(results$hit, ";", sep="")) # temp fix to end with ;....get rid of sizeout!!!
+results <- data.frame(otu=names(otus), nw.hits=best, stringsAsFactors=FALSE)
 
-# Get percent identity of global alignment of each sequence with its best hit from database
-
-# align each otu sequence to its best needle hit, using same parameters as command line needle
-# "global-local" forces whole reference/pattern sequence to be used, but subject/query can be a concescutive subsequence -- this is because queries may include slightly longer region of gene than references because references were trimmed by o-smart-trim. this means that query sequence can have more bases than the reference at beginning or end of the sequence (=end gaps in alignment) without penalties.
-psa <- pairwiseAlignment(subject=otus[results$otu], pattern=ref[results$hit], type="global-local",
+# Get percent similarity of global alignment of each sequence with its best hit from database
+# If multiple best hits, select first one for comparison
+results$nw.hit1 <- unlist(lapply(strsplit(results$nw.hits, split=";"), "[", 1))
+# Align each otu sequence to its best hit
+psa <- pairwiseAlignment(subject=otus[results$otu], pattern=ref[results$nw.hit1], type="global-local",
                          substitutionMatrix=EDNAFULL,
                          gapOpening=10.0, gapExtension=0.5)
 
-# calculate percent similarity to reference with indels counting as single difference regardless of length
+# Calculate percent similarity to reference with indels counting as single difference regardless of length
 results <- within(results, {
-  hit.length <- width(pattern(psa))
-  mismatches <- nmismatch(psa)
-  insertions <- unlist(lapply(as.list(insertion(psa)), length))
-  deletions <- unlist(lapply(as.list(deletion(psa)), length))
-  similarity <- round((hit.length-mismatches-insertions-deletions)/hit.length, 4)
+  nw.len <- width(pattern(psa))
+  nw.mis <- nmismatch(psa)
+  nw.ins <- unlist(lapply(as.list(insertion(psa)), length))
+  nw.del <- unlist(lapply(as.list(deletion(psa)), length))
+  nw.sim <- round((nw.len-nw.mis-nw.ins-nw.del)/nw.len, 4) * 100
 })
 
 # Write results to .tsv file
