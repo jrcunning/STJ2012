10c3ec88ddb1a039344d007214771d28bad6ce3d
Ross Cunning <ross.cunning@gmail.com>
2016-09-06 14:41:25 -1000

    fix code to keep names of poor matches that do not get a successful BLAST hit

--- Shell/run_blast_poormatches.sh
+++ Shell/run_blast_poormatches.sh
@@ -3,13 +3,16 @@
 dir=$(dirname $1)
 
 # Get names of otus whose representative sequence is <90% similar to best hit
-awk '$3 < 90 {print $2}' $1 > $dir/poor_matches.txt
+awk '$3 < 90 {print $2}' $1 | sort -u > $dir/poor_matches.txt
 # Get corresponding sequences
 awk 'NR==FNR {a[">"$1]; next} $1 in a {print; getline; print}' $dir/poor_matches.txt $2 > $dir/poor_matches_seqs.fasta
 
 # Blast sequences to NCBI nr databse
-blastn -db nr -remote -query $dir/poor_matches_seqs.fasta -outfmt '6 qseqid qcovs stitle sseqid' -max_target_seqs 1 | sort -u > $dir/poormatch_IDs.txt
+blastn -db nr -remote -query $dir/poor_matches_seqs.fasta -outfmt '6 qseqid qcovs stitle sseqid' -max_target_seqs 1 | sort -u > $dir/poormatch_IDs1.txt
+
+join -a 1 $dir/poor_matches.txt $dir/poormatch_IDs1.txt > $dir/poormatch_IDs.txt
 
 # Clean up
 rm $dir/poor_matches.txt
 rm $dir/poor_matches_seqs.fasta
+rm $dir/poormarch_IDs1.txt
