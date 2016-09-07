4b487e98b0d0340d4a6c67168622da60b34d03a5
jrcunning <ross.cunning@gmail.com>
2016-09-06 15:09:30 -1000

    fix poormatch blast to track those with no hits

--- Shell/run_blast_poormatches.sh
+++ Shell/run_blast_poormatches.sh
@@ -8,11 +8,11 @@ awk '$3 < 90 {print $2}' $1 | sort -u > $dir/poor_matches.txt
 awk 'NR==FNR {a[">"$1]; next} $1 in a {print; getline; print}' $dir/poor_matches.txt $2 > $dir/poor_matches_seqs.fasta
 
 # Blast sequences to NCBI nr databse
-blastn -db nr -remote -query $dir/poor_matches_seqs.fasta -outfmt '6 qseqid qcovs stitle sseqid' -max_target_seqs 1 | sort -u > $dir/poormatch_IDs1.txt
+blastn -db nr -remote -query $dir/poor_matches_seqs.fasta -outfmt '6 qseqid qcovs stitle sseqid' -max_target_seqs 1 | sort -u -k1,1 > $dir/poormatch_IDs1.txt
 
 join -a 1 $dir/poor_matches.txt $dir/poormatch_IDs1.txt > $dir/poormatch_IDs.txt
 
 # Clean up
 rm $dir/poor_matches.txt
 rm $dir/poor_matches_seqs.fasta
-rm $dir/poormarch_IDs1.txt
+rm $dir/poormatch_IDs1.txt
