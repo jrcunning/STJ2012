7e32eca17dd82da342625e24ebf20fa229033654
Ross Cunning <ross.cunning@gmail.com>
2016-12-01 13:56:46 -1000

    set betadisper bias.adjust=F (causes downstream error in new vegan version)

--- R/functions.R
+++ R/functions.R
@@ -3,17 +3,17 @@ betad <- function(phy, group) {
   samdat <- data.frame(sample_data(phy))
   braydist <- vegdist(t(otu_table(phy)), method="bray")
   sambd <- betadisper(d=braydist, group=samdat[, group], 
-                      type="centroid", bias.adjust=TRUE)
+                      type="centroid", bias.adjust=FALSE)
   # Calculate each species' mean distance to centroid and standard error
-  sambdsumm <- aggregate(data.frame(mean=sambd$distances), by=list(group=sambd$group), FUN=mean)
-  sambdsumm$se <- aggregate(sambd$distances, by=list(group=sambd$group), 
+  sambdsumm <- aggregate(data.frame(mean=as.vector(sambd$distances)), by=list(group=sambd$group), FUN=mean)
+  sambdsumm$se <- aggregate(as.vector(sambd$distances), by=list(group=sambd$group), 
                             FUN=function(x) sd(x)/sqrt(length(x)))$x
   # Determine order of decreasing mean dispersion
   sambdsumm.ord <- sambdsumm[order(sambdsumm$mean, decreasing=T), ]
   # Update betadisper object with species in decreasing order of mean dispersion
   sambd <- betadisper(d=braydist, 
                       group=factor(samdat[, group], levels=as.character(sambdsumm.ord$group)), 
-                      type="centroid", bias.adjust=TRUE)
+                      type="centroid", bias.adjust=FALSE)
   # Use permutations to perform pairwise comparisons of group mean dispersions
   sampt <- permutest(sambd, pairwise=T)
   saml <- multcompLetters(sampt$pairwise$permuted)
