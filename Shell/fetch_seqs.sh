a6e5722bba964a3d9cff81d521da55598842b789
Ross Cunning <ross.cunning@gmail.com>
2017-06-06 10:35:22 -0400

    update for NCBI https

--- Shell/fetch_seqs.sh
+++ Shell/fetch_seqs.sh
@@ -5,7 +5,7 @@ getseq() {
 	n=$1
 	acn=${n##*_}
 	echo $1 > data/dbseqs/${acn}.fasta
-	curl "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=${acn}&rettype=fasta" >> data/dbseqs/${acn}.fasta
+	curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=${acn}&rettype=fasta" >> data/dbseqs/${acn}.fasta
 }
 export -f getseq
 rm -rf data/dbseqs; mkdir data/dbseqs
@@ -14,7 +14,7 @@ parallel -a data/accn_nos.txt getseq
 # Put all sequences together and reformat to keep only subtype_accn as sequence name, and put sequence all on one line
 cat data/dbseqs/*.fasta | \
 awk '!/^>/ { printf "%s", $0; n = "\n" } /^>/ { print n $0; n = "" } END { printf "%s", n }' | \
-awk '!/^>gi/ {print}' > data/ITS2db_raw.fasta
+awk '!/\ / {print}' > data/ITS2db_raw.fasta
 
 # Clean up
 rm -rf data/dbseqs
